0f8ddef9e7bdcda78da9da0a90f13c6a10606891
jhlin2337 <James@jamess-air.gateway.pace.com>
2017-08-24 18:06:34 -0700

    web hosting

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ function Blob(id, x, y) {
 
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 5000);
 app.use(express.static('public'));
 
 console.log("My socket server is running");
